1408c00e24ad35183dfbe63b456c09f1e893d3a0
Reader Man <san_kan1gb@yahoo.co.uk>
2016-05-16 14:50:31 +0300

    Resource 2

---
+++ ResourceDiffrent.cs
@@ -0,0 +1,229 @@
+#pragma warning disable 1591
+//------------------------------------------------------------------------------
+// ResourceDiffrent.cs
+// This file has partial Classes with the missing constants of "Resource.Designer.cs", to stop the compiler from giving too many errors.
+// You can add more missing constants if you have errors after adding this file to your .Droid project
+// Refer to article: https://readerman1.wordpress.com/2016/05/11/xamarin-build-action-embeddedresource-is-not-supported-by-one-or-more-of-the-projects-targets/
+// the numbers are far bigger that the generated ones, to make sure that they will not collide with the automatically generated numbers for other constants
+//------------------------------------------------------------------------------
+
+
+//don’t forget to change the namespace from “SampleBrowser.Droid” to the one used in the “Resource.Designer.cs”.
+namespace SampleBrowser.Droid
+{
+
+
+    public partial class Resource
+    {
+
+        public partial class Attribute
+        {
+            
+            public const int mediaRouteSettingsDrawable = 2141000000;
+
+        }
+        
+        public partial class Color
+        {
+
+            public const int design_textinput_error_color = 2141000001;
+
+        }
+        
+        public partial class Dimension
+        {
+
+            public const int design_fab_content_size = 2141000002;
+            
+            public const int design_navigation_padding_top_default = 2141000003;
+            public const int design_tab_min_width = 2141000004;
+            public const int dialog_fixed_height_major = 2141000005;
+            public const int dialog_fixed_height_minor = 2141000006;
+            public const int dialog_fixed_width_major = 2141000007;
+            public const int dialog_fixed_width_minor = 2141000008;
+            public const int mr_media_route_controller_art_max_height = 2141000009;
+
+        }
+
+        public partial class Drawable
+        {
+
+            public const int ic_setting_dark = 2141000010;
+            public const int ic_setting_light = 2141000011;
+            public const int mr_ic_settings_dark = 2141000012;
+            public const int mr_ic_settings_light = 2141000013;
+
+        }
+
+        public partial class Id
+        {
+
+            public const int art = 2141000014;
+            public const int buttons = 2141000015;
+            public const int default_control_frame = 2141000016;
+            public const int disconnect = 2141000017;
+            public const int media_route_control_frame = 2141000018;
+            public const int media_route_list = 2141000019;
+            public const int media_route_volume_layout = 2141000020;
+            public const int media_route_volume_slider = 2141000021;
+            public const int play_pause = 2141000022;
+            public const int route_name = 2141000023;
+            public const int settings = 2141000024;
+            public const int stop = 2141000025;
+            public const int subtitle = 2141000026;
+            public const int title_bar = 2141000027;
+
+        }
+        
+        public partial class Layout
+        {
+
+            public const int mr_media_route_chooser_dialog = 2141000028;
+            public const int mr_media_route_controller_material_dialog_b = 2141000029;
+            public const int mr_media_route_list_item = 2141000030;
+
+        }
+
+        public partial class String
+        {
+
+            public const int mr_media_route_button_content_description = 2141000031;
+            public const int mr_media_route_chooser_searching = 2141000032;
+            public const int mr_media_route_chooser_title = 2141000033;
+            public const int mr_media_route_controller_disconnect = 2141000034;
+            public const int mr_media_route_controller_no_info_available = 2141000035;
+            public const int mr_media_route_controller_pause = 2141000036;
+            public const int mr_media_route_controller_play = 2141000037;
+            public const int mr_media_route_controller_settings_description = 2141000038;
+            public const int mr_media_route_controller_stop = 2141000039;
+
+        }
+
+        public partial class Style
+        {
+            public const int RtlOverlay_Widget_AppCompat_ActionButton_Overflow = 2141000040;
+
+        }
+
+        public partial class Styleable
+        {
+            public const int FloatingActionButton_android_background = 0;
+            public const int Theme_radioButtonStyle = 104;
+            public const int Theme_ratingBarStyle = 105;
+            public const int Theme_searchViewStyle = 67;
+            public const int Theme_selectableItemBackground = 52;
+            public const int Theme_selectableItemBackgroundBorderless = 53;
+            public const int Theme_spinnerDropDownItemStyle = 47;
+			public const int Theme_spinnerStyle = 106;
+			
+			public const int Theme_switchStyle = 107;
+			public const int Theme_textAppearanceLargePopupMenu = 40;
+			public const int Theme_textAppearanceListItem = 75;
+			public const int Theme_textAppearanceListItemSmall = 76;
+			public const int Theme_textAppearanceSearchResultSubtitle = 65;
+			public const int Theme_textAppearanceSearchResultTitle = 64;
+			public const int Theme_textAppearanceSmallPopupMenu = 41;
+			public const int Theme_textColorAlertDialogListItem = 94;
+			public const int Theme_textColorSearchUrl = 66;
+			public const int Theme_toolbarNavigationButtonStyle = 59;
+			public const int Theme_toolbarStyle = 58;
+			public const int Theme_windowActionBar = 2;
+			public const int Theme_windowActionBarOverlay = 4;
+			public const int Theme_windowActionModeOverlay = 5;
+			public const int Theme_windowFixedHeightMajor = 9;
+			public const int Theme_windowFixedHeightMinor = 7;
+			public const int Theme_windowFixedWidthMajor = 6;
+			public const int Theme_windowFixedWidthMinor = 8;
+			public const int Theme_windowMinWidthMajor = 10;
+			public const int Theme_windowMinWidthMinor = 11;
+			public const int Theme_windowNoTitle = 3;
+
+            public static int[] Theme = new int[] { 0, 1};
+
+			public const int Theme_actionBarDivider = 23;
+			public const int Theme_actionBarItemBackground = 24;
+			public const int Theme_actionBarPopupTheme = 17;
+			public const int Theme_actionBarSize = 22;
+			public const int Theme_actionBarSplitStyle = 19;
+			public const int Theme_actionBarStyle = 18;
+			public const int Theme_actionBarTabBarStyle = 13;
+			public const int Theme_actionBarTabStyle = 12;
+			public const int Theme_actionBarTabTextStyle = 14;
+			public const int Theme_actionBarTheme = 20;
+			public const int Theme_actionBarWidgetTheme = 21;
+			public const int Theme_actionButtonStyle = 49;
+			public const int Theme_actionDropDownStyle = 45;
+			public const int Theme_actionMenuTextAppearance = 25;
+			public const int Theme_actionMenuTextColor = 26;
+			public const int Theme_actionModeBackground = 29;
+			public const int Theme_actionModeCloseButtonStyle = 28;
+			public const int Theme_actionModeCloseDrawable = 31;
+			public const int Theme_actionModeCopyDrawable = 33;
+			public const int Theme_actionModeCutDrawable = 32;
+			public const int Theme_actionModeFindDrawable = 37;
+			public const int Theme_actionModePasteDrawable = 34;
+			public const int Theme_actionModePopupWindowStyle = 39;
+			public const int Theme_actionModeSelectAllDrawable = 35;
+			public const int Theme_actionModeShareDrawable = 36;
+			public const int Theme_actionModeSplitBackground = 30;
+			public const int Theme_actionModeStyle = 27;
+			public const int Theme_actionModeWebSearchDrawable = 38;
+			public const int Theme_actionOverflowButtonStyle = 15;
+			public const int Theme_actionOverflowMenuStyle = 16;
+			public const int Theme_activityChooserViewStyle = 57;
+			public const int Theme_alertDialogButtonGroupStyle = 91;
+			public const int Theme_alertDialogCenterButtons = 92;
+			public const int Theme_alertDialogStyle = 90;
+			public const int Theme_alertDialogTheme = 93;
+			public const int Theme_android_windowAnimationStyle = 1;
+			public const int Theme_android_windowIsFloating = 0;
+			public const int Theme_autoCompleteTextViewStyle = 98;
+			public const int Theme_borderlessButtonStyle = 54;
+			public const int Theme_buttonBarButtonStyle = 51;
+			public const int Theme_buttonBarNegativeButtonStyle = 96;
+			public const int Theme_buttonBarNeutralButtonStyle = 97;
+			public const int Theme_buttonBarPositiveButtonStyle = 95;
+			public const int Theme_buttonBarStyle = 50;
+			public const int Theme_buttonStyle = 99;
+			public const int Theme_buttonStyleSmall = 100;
+			public const int Theme_checkboxStyle = 101;
+			public const int Theme_checkedTextViewStyle = 102;
+			public const int Theme_colorAccent = 83;
+			public const int Theme_colorButtonNormal = 87;
+			public const int Theme_colorControlActivated = 85;
+			public const int Theme_colorControlHighlight = 86;
+			public const int Theme_colorControlNormal = 84;
+			public const int Theme_colorPrimary = 81;
+			public const int Theme_colorPrimaryDark = 82;
+			public const int Theme_colorSwitchThumbNormal = 88;
+			public const int Theme_controlBackground = 89;
+			public const int Theme_dialogPreferredPadding = 43;
+			public const int Theme_dialogTheme = 42;
+			public const int Theme_dividerHorizontal = 56;
+			public const int Theme_dividerVertical = 55;
+			public const int Theme_dropDownListViewStyle = 73;
+			public const int Theme_dropdownListPreferredItemHeight = 46;
+			public const int Theme_editTextBackground = 63;
+			public const int Theme_editTextColor = 62;
+			public const int Theme_editTextStyle = 103;
+			public const int Theme_homeAsUpIndicator = 48;
+			public const int Theme_listChoiceBackgroundIndicator = 80;
+			public const int Theme_listDividerAlertDialog = 44;
+			public const int Theme_listPopupWindowStyle = 74;
+			public const int Theme_listPreferredItemHeight = 68;
+			public const int Theme_listPreferredItemHeightLarge = 70;
+			public const int Theme_listPreferredItemHeightSmall = 69;
+			public const int Theme_listPreferredItemPaddingLeft = 71;
+			public const int Theme_listPreferredItemPaddingRight = 72;
+			public const int Theme_panelBackground = 77;
+			public const int Theme_panelMenuListTheme = 79;
+			public const int Theme_panelMenuListWidth = 78;
+			public const int Theme_popupMenuStyle = 60;
+			public const int Theme_popupWindowStyle = 61;
+
+
+        }
+ 
+
+    }
+}
